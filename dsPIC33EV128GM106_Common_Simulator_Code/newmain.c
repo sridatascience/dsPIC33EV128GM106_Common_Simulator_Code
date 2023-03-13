c01c756d5e1853d7eb192c8cc8e7ff2888b631a1
sridhar vumma <sridhar6358@gmail.com>
2023-03-13 13:57:04 +0530

    unzipped version

---
+++ dsPIC33EV128GM106_Common_Simulator_Code/newmain.c
@@ -0,0 +1,17 @@
+#include <xc.h>
+#pragma config ICS = PGD3              // ICD Communication Channel Select bits (Communicate on PGC3 and PGD3)
+volatile uint8_t m =10;
+uint8_t add(uint8_t n, uint8_t o, uint8_t p ){
+   return n+o+p;
+}
+void main() {
+   volatile uint8_t a = 0x05;
+   TMR1 = a;
+   volatile uint8_t c = 0x1F;
+   PR1 = c;
+   volatile uint8_t b =0x08;
+   TMR2= b;
+   volatile uint8_t t = add(a,b,c);
+   PR2 = t;
+   return;
+}
