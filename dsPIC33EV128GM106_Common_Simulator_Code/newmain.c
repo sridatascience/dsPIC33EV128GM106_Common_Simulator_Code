d62e7bc545074d4696c32bd28ecb1c003d644c65
sridhar vumma <50115187+sridatascience@users.noreply.github.com>
2023-04-10 19:22:09 +0530

    Update newmain.c

--- dsPIC33EV128GM106_Common_Simulator_Code/newmain.c
+++ dsPIC33EV128GM106_Common_Simulator_Code/newmain.c
@@ -7,6 +7,7 @@ uint8_t add(uint8_t n, uint8_t o, uint8_t p ){
 void main() {
    volatile uint8_t a = 0x05;
    TMR1 = a;
+   m = TMR1;
    volatile uint8_t c = 0x1F;
    PR1 = c;
    volatile uint8_t b =0x08;
